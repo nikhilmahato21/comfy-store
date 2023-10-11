5fa6671ed40f51b4e65c6577f4d484d51820e881
nikhil <nikhilmahato3337@gmail.com>
2023-10-11 21:36:31 +0530

    added error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useRouteError, Link } from "react-router-dom";
 
 const Error = () => {
-  return <h1>error!</h1>;
+  const error = useRouteError();
+  console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <main className=" grid min-h-screen place-items-center px-8">
+        <div className="text-center">
+          <p className="text-9xl font-semibold text-primary">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
+            page not found
+          </h1>
+          <p className="mt-6 txt-lg leading-7">
+            Sorry, we couldn't find the page you're looking for.
+          </p>
+          <div className="mt-10">
+            <Link to="/" className="btn btn-secondary">
+              Go Back Home
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main className=" grid min-h-screen place-items-center px-8">
+      <h1 className="text-center font-bold text-4xl">error occured</h1>
+    </main>
+  );
 };
 
 export default Error;
