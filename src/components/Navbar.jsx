0bf0396db27f6a9b402e5e622c1136ee5d197e1a
nikhil <nikhilmahato3337@gmail.com>
2023-10-12 23:40:26 +0530

    navbar function

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import { FaBarsStaggered } from "react-icons/fa6";
+import { BsCart3 } from "react-icons/bs";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-base-200">
+      <div className="navbar align-element">
+        <div className="navbar-start">
+          <NavLink to="/" className="hidden lg:flex btn btn-primary text-3xl">
+            C
+          </NavLink>
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <FaBarsStaggered className="h-6 w-6" />
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200"
+            >
+              navlinks
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal">navlinks</ul>
+        </div>
+        <div className="navbar-end">
+          <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
+            <div className="indicator">
+              <BsCart3 className="h-6 w-6" />
+              <span className="badge badge-sm badge-primary indicator-item">
+                0
+              </span>
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 const HomeLayout = () => {
   return (
     <>
       <Header />
+      <Navbar />
       <section className="align-element py-20">
         <Outlet />
       </section>
