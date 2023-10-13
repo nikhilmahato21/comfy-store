332944875a24073d37539d32b8970965e7ba7e8b
nikhil <nikhilmahato3337@gmail.com>
2023-10-13 07:16:42 +0530

    navlinks and dropdown navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { FaBarsStaggered } from "react-icons/fa6";
 import { BsCart3 } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
+import NavLinks from "./NavLinks";
 
 const Navbar = () => {
   return (
@@ -16,14 +17,16 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
             >
-              navlinks
+              <NavLinks />
             </ul>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal">navlinks</ul>
+          <ul className="menu menu-horizontal">
+            <NavLinks />
+          </ul>
         </div>
         <div className="navbar-end">
           <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
