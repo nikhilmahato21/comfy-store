29a933afeabac184b47bd5dc736df228ab2e47bb
nikhil <nikhilmahato3337@gmail.com>
2023-10-17 13:20:34 +0530

    toggle filter added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,9 @@
+import { LuSettings2 } from "react-icons/lu";
 import Filters from "../components/Filters";
 import PaginationContainer from "../components/PaginationContainer";
 import ProductsContainer from "../components/ProductsContainer";
 import { customFetch } from "../utils";
+import { useState } from "react";
 
 export const loader = async ({ request }) => {
   const response = await customFetch("/products");
@@ -11,9 +13,20 @@ export const loader = async ({ request }) => {
 };
 
 const Products = () => {
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
   return (
     <>
-      <Filters />
+      <div className="flex justify-between items-center  mb-3">
+        <button
+          className="btn btn-ghost btn-circle btn-md ml-auto p-3"
+          onClick={() => {
+            setIsFilterOpen(!isFilterOpen);
+          }}
+        >
+          <LuSettings2 className="h-6 w-6" />
+        </button>
+      </div>
+      {isFilterOpen && <Filters />}
       <ProductsContainer />
       <PaginationContainer />
     </>
