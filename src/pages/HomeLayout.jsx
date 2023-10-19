2a79db20f71033602967c775c316e74687c66c91
nikhil <nikhilmahato3337@gmail.com>
2023-10-19 15:56:48 +0530

    loading spinner added

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,14 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
+import Loading from "../components/Loading";
 const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
   return (
     <>
       <Header />
       <Navbar />
-      <section className="align-element py-20">
-        <Outlet />
-      </section>
+      {isPageLoading ? (
+        <Loading />
+      ) : (
+        <section className="align-element py-20">
+          <Outlet />
+        </section>
+      )}
     </>
   );
 };
