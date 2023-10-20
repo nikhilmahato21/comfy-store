1bdddf5251990eeb6c841dd3247f8c515920133e
nikhil <nikhilmahato3337@gmail.com>
2023-10-20 14:55:18 +0530

    pagination function

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
 
 const PaginationContainer = () => {
   const { meta } = useLoaderData();
   const { pageCount, page } = meta.pagination;
+
   const pages = Array.from({ length: pageCount }, (_, index) => {
     return index + 1;
   });
+
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
   const handlePageChange = (pageNumber) => {
     console.log(pageNumber);
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
   };
   if (pageCount < 2) return null;
   return (
@@ -16,7 +23,11 @@ const PaginationContainer = () => {
       <div className="join">
         <button
           className="btn btn-xs sm:btn-md join-item"
-          onClick={handlePageChange("prev")}
+          onClick={() => {
+            let prevPage = page - 1;
+            if (prevPage < 1) prevPage = pageCount;
+            handlePageChange(prevPage);
+          }}
         >
           Prev
         </button>
@@ -35,7 +46,11 @@ const PaginationContainer = () => {
         })}
         <button
           className="btn btn-xs sm:btn-md join-item"
-          onClick={handlePageChange("next")}
+          onClick={() => {
+            let nextPage = page + 1;
+            if (nextPage > pageCount) nextPage = 1;
+            handlePageChange(nextPage);
+          }}
         >
           Next
         </button>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,3 @@
-import { LuSettings2 } from "react-icons/lu";
 import Filters from "../components/Filters";
 import PaginationContainer from "../components/PaginationContainer";
 import ProductsContainer from "../components/ProductsContainer";
@@ -9,7 +8,7 @@ export const loader = async ({ request }) => {
   const params = Object.fromEntries([
     ...new URL(request.url).searchParams.entries(),
   ]);
-  console.log(params);
+
   const response = await customFetch("/products", { params });
   const products = response.data.data;
   const meta = response.data.meta;
