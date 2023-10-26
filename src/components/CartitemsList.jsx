22738869f69391b8ba79946a92e642d80d8d80c0
nikhil <nikhilmahato3337@gmail.com>
2023-10-26 15:38:18 +0530

    cart items setup

--- src/components/CartitemsList.jsx
+++ src/components/CartitemsList.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import CartItem from "./CartItem";
 
 const CartitemsList = () => {
-  return <div>CartitemsList</div>;
+  const cartItems = useSelector((state) => state.cartState.cartItems);
+  return (
+    <>
+      {cartItems.map((item) => {
+        return <CartItem key={item.cartId} cartItem={item} />;
+      })}
+    </>
+  );
 };
 
 export default CartitemsList;
