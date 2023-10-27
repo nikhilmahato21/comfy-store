1d7550ce7dc0572cf21d29ae5f3a2cf10dde4a16
nikhil <nikhilmahato3337@gmail.com>
2023-10-27 23:00:35 +0530

    logout feature added

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -24,7 +24,9 @@ const userSlice = createSlice({
       console.log("login");
     },
     logoutUser: (state) => {
-      console.log("logout");
+      state.user = null;
+      localStorage.removeItem("user");
+      toast.success("Logged out successfully");
     },
     toggleTheme: (state) => {
       const { dracula, winter } = themes;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,7 +5,7 @@ import CartTotals from "../components/CartTotals";
 import { Link } from "react-router-dom";
 
 const Cart = () => {
-  const user = null;
+  const user = useSelector((state) => state.userState.user);
   const numItemsInCart = useSelector((state) => state.cartState.numItemsInCart);
 
   if (numItemsInCart === 0) {
