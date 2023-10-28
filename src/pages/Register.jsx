60da302d8ecc6e1682225019bb3efd5f11031885
nikhil <nikhilmahato3337@gmail.com>
2023-10-28 12:30:38 +0530

    register user done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { Form, Link } from "react-router-dom";
+import { Form, Link, redirect } from "react-router-dom";
 import FormInput from "../components/FormInput";
 import SubmitBtn from "../components/SubmitBtn";
+import { customFetch } from "../utils";
+import { toast } from "react-toastify";
 
-export const action = async () => {
-  return null;
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    const response = await customFetch.post("/auth/local/register", data);
+    toast.success("account created successfully");
+    return redirect("/login");
+  } catch (error) {
+    const errorMessage =
+      error?.response?.data?.error?.message ||
+      "please double check your credentials";
+    toast.error(errorMessage);
+    return null;
+  }
 };
 
 const Register = () => {
@@ -15,9 +30,24 @@ const Register = () => {
         className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
       >
         <h4 className="text-center text-3xl font-bold">Register</h4>
-        <FormInput type="text" label="username" name="username" />
-        <FormInput type="email" label="email" name="email" />
-        <FormInput type="password" label="password" name="password" />
+        <FormInput
+          type="text"
+          label="username"
+          name="username"
+          defaultValue="nik"
+        />
+        <FormInput
+          type="email"
+          label="email"
+          name="email"
+          defaultValue="nik@gmail.com"
+        />
+        <FormInput
+          type="password"
+          label="password"
+          name="password"
+          defaultValue="secret"
+        />
         <div className="mt-4">
           <SubmitBtn text="register" />
         </div>
