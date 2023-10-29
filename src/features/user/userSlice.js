e28d93268d601b2e829d1a63ab0c54b2a9712253
nikhil <nikhilmahato3337@gmail.com>
2023-10-29 23:57:37 +0530

    login user reducer

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,13 +6,17 @@ const themes = {
   dracula: "dracula",
 };
 
+const getUserFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem("user")) || null;
+};
+
 const getThemeFromLocalStorage = () => {
   const theme = localStorage.getItem("theme") || themes.winter;
   document.documentElement.setAttribute("data-theme", theme);
   return theme;
 };
 const initialState = {
-  user: { username: "nikhil" },
+  user: getUserFromLocalStorage(),
   theme: getThemeFromLocalStorage(),
 };
 
@@ -21,7 +25,9 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     loginUser: (state, action) => {
-      console.log(action.payload);
+      const user = { ...action.payload.user, token: action.payload.jwt };
+      state.user = user;
+      localStorage.setItem("user", JSON.stringify(user));
     },
     logoutUser: (state) => {
       state.user = null;
